2e8216c956c90913926e790df2ecd2440b062583
Craig Varrichio <cvarrichio@skeinhealth.com>
2023-05-03 10:36:57 -0500

    Attempted websockets version.

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import StreamingResponse
 from dotenv import load_dotenv
-load_dotenv(override=True)
+import json
 
-
+load_dotenv()
 
 from models import chat_model, question_model
-
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -28,36 +26,50 @@ class MessageInput(BaseModel):
 class UpdateMemoryInput(BaseModel):
     question: str
     answer: str
-    
-@app.post("/update_memory")
-async def update_memory_endpoint(data: UpdateMemoryInput):
-    chat_model.update_memory(data.question, data.answer)
-    logging.debug('Updating memory!')
-    return {"status": "ok"}
-
-
-@app.post("/get_questions")
-async def get_questions(message_data: MessageInput):
-    question = message_data.message
-    response = question_model.create(question)
-    return {"status": "ok"}
-
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+ 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
 
+    while True:
+        try:
+            data = await websocket.receive_json()
 
-@app.post("/submit_message")
-async def submit_message(message_data: MessageInput):
-    question = message_data.message
-    message = '<strong>' + question + '</strong>'
-    MODEL = 'gpt-3.5-turbo'
-    return StreamingResponse(
-        chat_model.stream_messages(MODEL,question),
-        media_type="text/plain",
-    )
+            if data['type'] == 'submit_message':
+                question = data['message']
+                async for message in chat_model.stream_messages('gpt-3.5-turbo', question):
+                    await websocket.send_json({
+                        'type': 'message',
+                        'message': message
+                    })
 
+                #updateMemory(question, message)
+                await websocket.send_json({
+                    'type': 'memory_updated'
+                })
+
+            elif data['type'] == 'get_questions':
+                question = data['message']
+                response = question_model.create(question)
+                await websocket.send_json({
+                    'type': 'questions_received'
+                })
+
+            elif data['type'] == 'update_memory':
+                question = data['question']
+                answer = data['answer']
+                chat_model.update_memory(question, answer)
+                await websocket.send_json({
+                    'type': 'memory_updated'
+                })
+
+        except Exception as e:
+            print(f"Error: {e}")
+            break
 
 if __name__ == "__main__":
     import uvicorn
